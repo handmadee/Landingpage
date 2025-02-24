47ed9e947c169065c1b58708ce8d8574016d434e
Dino_datcoder38 <dinodatcoder38@gmail.com>
2025-02-24 08:45:54 +0700

    firt commit

---
+++ ladinpage/main.js
@@ -0,0 +1,27 @@
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     window.chatwootSettings = { "position": "right", "type": "standard", "launcherTitle": "Chat with us" };
+//     (function (d, t) {
+//         console.log("loading")
+//         var BASE_URL = "http://0.0.0.0:3000";
+//         var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+//         g.src = BASE_URL + "/packs/js/sdk.js";
+//         g.defer = true;
+//         g.async = true;
+//         s.parentNode.insertBefore(g, s);
+//         console.log("loading {{")
+//         try {
+//             g.onload = function () {
+//                 console.log("loading {{")
+//                 window.chatwootSDK.run({
+//                     websiteToken: '8sHWu8FbmLm5Bc45ma6izyho',
+//                     baseUrl: BASE_URL
+//                 })
+//             }
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     })(document, "script");
+
+
+// });
